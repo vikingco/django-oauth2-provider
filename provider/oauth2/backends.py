3ceee2f7abeb0313de557c8970fda51705d87091
Linsey Raymaekers <linsey.raymaekers@vikingco.com>
2016-03-29 15:20:08 +0200

    The WSGIRequest.REQUEST attribute has been removed

--- provider/oauth2/backends.py
+++ provider/oauth2/backends.py
@@ -53,7 +53,7 @@ class RequestParamsClientBackend(object):
         if request is None:
             return None
 
-        form = ClientAuthForm(request.REQUEST)
+        form = ClientAuthForm(request.POST if request.method == 'POST' else request.GET)
 
         if form.is_valid():
             return form.cleaned_data.get('client')
@@ -74,7 +74,7 @@ class PublicPasswordBackend(object):
         if request is None:
             return None
 
-        form = PublicPasswordGrantForm(request.REQUEST)
+        form = PublicPasswordGrantForm(request.POST if request.method == 'POST' else request.GET)
 
         if form.is_valid():
             return form.cleaned_data.get('client')
--- provider/oauth2/tests.py
+++ provider/oauth2/tests.py
@@ -462,10 +462,10 @@ class AuthBackendTest(BaseOAuth2TestCase):
                          2, "Didn't return the right client.")
 
     def test_request_params_client_backend(self):
-        request = type('Request', (object,), {'REQUEST': {}})()
+        request = type('Request', (object,), {'GET': {}})()
 
-        request.REQUEST['client_id'] = self.get_client().client_id
-        request.REQUEST['client_secret'] = self.get_client().client_secret
+        request.GET['client_id'] = self.get_client().client_id
+        request.GET['client_secret'] = self.get_client().client_secret
 
         self.assertEqual(RequestParamsClientBackend().authenticate(request).id,
                          2, "Didn't return the right client.'")
